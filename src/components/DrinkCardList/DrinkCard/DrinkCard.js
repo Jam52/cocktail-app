29b1cd8802b8f53758c07136a59a352b9ea00e83
Jam52 <jamie.sajdak@gmail.com>
2020-08-24 13:09:25 -0400

    Feat: add hover effect on drinkCards

--- src/components/DrinkCardList/DrinkCard/DrinkCard.js
+++ src/components/DrinkCardList/DrinkCard/DrinkCard.js
@@ -9,7 +9,10 @@ const drinkCard = (props) => {
         <Link to={"/drinkdetails/" + props.id} className={classes.DrinkCard}>
             <img src={DrinkCardTopper}></img>
             <h2>{props.title}</h2>
-            <img src={props.src} />
+            <div className={classes.Container}>
+                <div className={classes.Overlay}></div>
+                <img src={props.src} />
+            </div>
         </Link>
     );
 };
