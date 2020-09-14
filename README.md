60e2f7f590fb169cdbce8d5d518e9aa0bc2a8a60
Jam52 <jamie.sajdak@gmail.com>
2020-09-14 14:47:13 -0400

    Docs: Update ReadMe

--- README.md
+++ README.md
@@ -1,68 +1,59 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Madam Mimi's Cocktail App
 
-## Available Scripts
+A single page web application to search for and explore cocktails.
+Uses [cocktaildb.com](https://www.thecocktaildb.com/) API database.
 
-In the project directory, you can run:
+## Built with
 
-### `npm start`
+- React
+- SCSS
+- JavaScript
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Installation
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+### Prerequisites
 
-### `npm test`
+- npm
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```sh
+npm install npm@latest -g
+```
 
-### `npm run build`
+### Installation
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+1. Get api by backing [cocktaildb](https://www.thecocktaildb.com/) on [patreon](https://www.patreon.com/home).
+2. Clone the repo
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+```sh
+git clone https://github.com/Jam52/cocktail-app.git
+```
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+3. Install NPM packages
 
-### `npm run eject`
+```sh
+npm install
+```
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+4. Enter your API key in `.env` file
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+```
+REACT_APP_COCKTAIL_KEY='Your Key Here'
+```
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+5. Run project
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+```sh
+npm start
+```
 
-## Learn More
+## Hosting
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+Hosted on [heroku](https://www.heroku.com/)
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+[View Site](https://vigilant-minsky-54c7d5.netlify.app/)
 
-### Code Splitting
+## Contact
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+Jamie Sajdak - jamie.sajdak@gmail.com
 
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+[Portfolio](https://www.jamiesajdak.com/)
--- src/components/DrinkCardList/DrinkCardList.js
+++ src/components/DrinkCardList/DrinkCardList.js
@@ -1,175 +1,169 @@
-import React, { Component } from "react";
-import classes from "./DrinkCardList.module.scss";
-import axios from "../../axiosCocktail";
-import DrinkCard from "./DrinkCard/DrinkCard";
-import Divider from "../../components/Divider/Divider";
-import Aux from "../../hoc/Auxillary/Auxillary";
-import Spinner from "../Spinner/Spinner";
+import React, { Component } from 'react';
+import classes from './DrinkCardList.module.scss';
+import axios from '../../axiosCocktail';
+import DrinkCard from './DrinkCard/DrinkCard';
+import Divider from '../../components/Divider/Divider';
+import Aux from '../../hoc/Auxillary/Auxillary';
+import Spinner from '../Spinner/Spinner';
 
 class DrinkCardList extends Component {
-    state = {
-        searchItems: "",
-        drinks: [],
-        loading: true,
-    };
+  state = {
+    searchItems: '',
+    drinks: [],
+    loading: true,
+  };
 
-    componentDidMount() {
-        console.log("[DrinkCardList]ComponentDidMount");
-        console.log(this.props.match.params.search);
-        if (
-            this.state.searchItems !==
-            this.props.match.params.search + this.loadSearchParams()
-        ) {
-            this.fetchData();
-        }
+  componentDidMount() {
+    console.log('[DrinkCardList]ComponentDidMount');
+    console.log(this.props.match.params.search);
+    if (
+      this.state.searchItems !==
+      this.props.match.params.search + this.loadSearchParams()
+    ) {
+      this.fetchData();
     }
+  }
 
-    componentDidUpdate() {
-        console.log("[DrinkCardList]ComponentDidUpdate");
-        if (
-            this.state.searchItems !==
-            this.props.match.params.search + this.loadSearchParams()
-        ) {
-            this.fetchData();
-        }
+  componentDidUpdate() {
+    console.log('[DrinkCardList]ComponentDidUpdate');
+    if (
+      this.state.searchItems !==
+      this.props.match.params.search + this.loadSearchParams()
+    ) {
+      this.fetchData();
     }
+  }
 
-    fetchData = async () => {
-        console.log(this.props.match.params.param + this.loadSearchParams());
-        let mainSearchParam = this.props.match.params.search.replace(" ", "_");
-        console.log("{MainSearchParam}", mainSearchParam);
+  fetchData = async () => {
+    console.log(this.props.match.params.param + this.loadSearchParams());
+    let mainSearchParam = this.props.match.params.search.replace(' ', '_');
+    console.log('{MainSearchParam}', mainSearchParam);
 
-        let mainData = [];
-        let data = ["inital"];
+    let mainData = [];
+    let data = ['inital'];
 
-        if (mainSearchParam !== "?i=") {
-            mainData = await axios
-                .get(
-                    "/v2/9973533/" +
-                        this.props.match.params.param +
-                        mainSearchParam
-                )
-                .then((response) => {
-                    console.log("[MainData]", response.data.drinks);
-                    if (response.data.drinks !== null) {
-                        return [...response.data.drinks];
-                    } else {
-                        return [];
-                    }
-                });
-            if (mainData.length > 0) {
-                data = [...mainData];
-            }
+    if (mainSearchParam !== '?i=') {
+      mainData = await axios
+        .get(
+          `/v2/${process.env.REACT_APP_COCKTAIL_KEY}/` +
+            this.props.match.params.param +
+            mainSearchParam,
+        )
+        .then((response) => {
+          console.log('[MainData]', response.data.drinks);
+          if (response.data.drinks !== null) {
+            return [...response.data.drinks];
+          } else {
+            return [];
+          }
+        });
+      if (mainData.length > 0) {
+        data = [...mainData];
+      }
 
-            if (typeof data[0] !== "object") {
-                this.setState({
-                    drinks: [],
-                    searchItems:
-                        this.props.match.params.search +
-                        this.loadSearchParams(),
-                    loading: false,
-                });
-                return null;
-            }
-        }
-
-        for (let param of this.loadSearchParams()) {
-            const fetchParam = await axios
-                .get("/v2/9973533/filter.php/" + param)
-                .then((response) => {
-                    console.log(
-                        `[ParamFetchData]${param}`,
-                        response.data.drinks
-                    );
-                    return [...response.data.drinks];
-                });
-
-            if (data.length === 0) {
-                break;
-            }
-
-            data = data.filter((obj) => typeof obj === "object");
-            data.push(...fetchParam);
-
-            if (data.length > fetchParam.length + 1) {
-                data = this.duplicateEntries(data);
-            }
-        }
-
-        console.log("{FINAL}", data);
-
+      if (typeof data[0] !== 'object') {
         this.setState({
-            drinks: data,
-            searchItems:
-                this.props.match.params.search + this.loadSearchParams(),
-            loading: false,
+          drinks: [],
+          searchItems: this.props.match.params.search + this.loadSearchParams(),
+          loading: false,
         });
-    };
-
-    duplicateEntries(data) {
-        return data
-            .sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1))
-            .filter((drink, index) => {
-                if (data[index + 1] !== undefined) {
-                    return drink.strDrink === data[index + 1].strDrink;
-                }
-            });
+        return null;
+      }
     }
 
-    fetchSearchParamsData = async (param) => {
-        axios.get("/v2/9973533/filter.php/" + param).then((response) => {
-            console.log(`[ParamFetchData]${param}`, response.data.drinks);
-
-            return response.data.drinks;
+    for (let param of this.loadSearchParams()) {
+      const fetchParam = await axios
+        .get(`/v2/${process.env.REACT_APP_COCKTAIL_KEY}/filter.php/` + param)
+        .then((response) => {
+          console.log(`[ParamFetchData]${param}`, response.data.drinks);
+          return [...response.data.drinks];
         });
-    };
 
-    loadSearchParams() {
-        const query = new URLSearchParams(this.props.location.search);
-        let newSearchParams = [];
-        for (let param of query.entries()) {
-            if (param[1]) {
-                newSearchParams.push(param.join("="));
-                console.log(newSearchParams);
-            }
-        }
-        return newSearchParams;
+      if (data.length === 0) {
+        break;
+      }
+
+      data = data.filter((obj) => typeof obj === 'object');
+      data.push(...fetchParam);
+
+      if (data.length > fetchParam.length + 1) {
+        data = this.duplicateEntries(data);
+      }
     }
 
-    render() {
-        let drinkCards = <Spinner />;
+    console.log('{FINAL}', data);
 
-        if (this.state.loading === false) {
-            if (
-                this.state.drinks.length > 0 &&
-                typeof this.state.drinks[0] === "object"
-            ) {
-                drinkCards = this.state.drinks.map((drink) => {
-                    return (
-                        <DrinkCard
-                            src={drink.strDrinkThumb}
-                            id={drink.idDrink}
-                            key={drink.idDrink}
-                            title={drink.strDrink}
-                        />
-                    );
-                });
-            } else {
-                drinkCards = (
-                    <p className={classes.Error}>
-                        Sorry, I Can't Find Anything!
-                    </p>
-                );
-            }
+    this.setState({
+      drinks: data,
+      searchItems: this.props.match.params.search + this.loadSearchParams(),
+      loading: false,
+    });
+  };
+
+  duplicateEntries(data) {
+    return data
+      .sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1))
+      .filter((drink, index) => {
+        if (data[index + 1] !== undefined) {
+          return drink.strDrink === data[index + 1].strDrink;
         }
+      });
+  }
 
-        return (
-            <Aux>
-                <div className={classes.DrinkCardList}>{drinkCards}</div>
-                <Divider className={classes.Divider} />
-            </Aux>
+  fetchSearchParamsData = async (param) => {
+    axios
+      .get(`/v2/${process.env.REACT_APP_COCKTAIL_KEY}/filter.php/` + param)
+      .then((response) => {
+        console.log(`[ParamFetchData]${param}`, response.data.drinks);
+
+        return response.data.drinks;
+      });
+  };
+
+  loadSearchParams() {
+    const query = new URLSearchParams(this.props.location.search);
+    let newSearchParams = [];
+    for (let param of query.entries()) {
+      if (param[1]) {
+        newSearchParams.push(param.join('='));
+        console.log(newSearchParams);
+      }
+    }
+    return newSearchParams;
+  }
+
+  render() {
+    let drinkCards = <Spinner />;
+
+    if (this.state.loading === false) {
+      if (
+        this.state.drinks.length > 0 &&
+        typeof this.state.drinks[0] === 'object'
+      ) {
+        drinkCards = this.state.drinks.map((drink) => {
+          return (
+            <DrinkCard
+              src={drink.strDrinkThumb}
+              id={drink.idDrink}
+              key={drink.idDrink}
+              title={drink.strDrink}
+            />
+          );
+        });
+      } else {
+        drinkCards = (
+          <p className={classes.Error}>Sorry, I Can't Find Anything!</p>
         );
+      }
     }
+
+    return (
+      <Aux>
+        <div className={classes.DrinkCardList}>{drinkCards}</div>
+        <Divider className={classes.Divider} />
+      </Aux>
+    );
+  }
 }
 
 export default DrinkCardList;
